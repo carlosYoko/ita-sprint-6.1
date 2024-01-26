85933e6de3a8da3d986e83b18b354f80abd9aaf2
CGimenez <carlosgibe@gmail.com>
2024-01-26 08:58:35 +0100

    initial express configuration

--- server/src/mysql/.endpoints copy.ts
+++ server/src/mysql/.endpoints copy.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
-import { PrismaClient } from '../../prisma/generated/client/';
+import { PrismaClient } from '../../prisma/generated/client';
 
 export const app = express();
 
