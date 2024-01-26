7ae11e034b0caf2f8f4d9de80153836e85e13860
CGimenez <carlosgibe@gmail.com>
2024-01-26 17:57:34 +0100

    refactoring

--- server/src/mysql/application/controllers/playerController.ts
+++ server/src/mysql/application/controllers/playerController.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from 'express';
-// import {
-//   findPlayerByName,
-//   createNewPlayer,
-// } from '../../infrastructure/repositories/playerRepository';
-import { createPlayer } from '../usecases/createPlayer';
+import {
+  findPlayerByName,
+  createNewPlayer,
+} from '../../infrastructure/repositories/playerRepository';
 import { PrismaClient } from '../../../../prisma/generated/client';
 
 export const createPlayerController = async (
@@ -14,35 +13,17 @@ export const createPlayerController = async (
   try {
     const { name } = req.body;
 
-    const newPlayer = await createPlayer(prisma, name);
+    const existingPlayer = await findPlayerByName(prisma, name);
 
-    res.status(200).send(newPlayer);
+    if (!existingPlayer) {
+      const newPlayer = await createNewPlayer(prisma, name);
+      res.status(200).send(newPlayer);
+    } else {
+      throw new Error('Ya existe un jugador con este nombre!');
+    }
   } catch (error) {
     if (error instanceof Error) {
       res.status(400).send({ message: error.message });
     }
   }
 };
-
-// export const createPlayerController = async (
-//   req: Request,
-//   res: Response,
-//   prisma: PrismaClient
-// ) => {
-//   try {
-//     const { name } = req.body;
-
-//     const existingPlayer = await findPlayerByName(prisma, name);
-
-//     if (!existingPlayer) {
-//       const newPlayer = await createNewPlayer(prisma, name);
-//       res.status(200).send(newPlayer);
-//     } else {
-//       throw new Error('Ya existe un jugador con este nombre!');
-//     }
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       res.status(400).send({ message: error.message });
-//     }
-//   }
-// };
