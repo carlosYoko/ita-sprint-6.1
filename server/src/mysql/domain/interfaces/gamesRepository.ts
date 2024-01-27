fe37311a54be41849d66f2fe7f19cfcfa2f25cdb
CGimenez <carlosgibe@gmail.com>
2024-01-27 15:29:15 +0100

    save

---
+++ server/src/mysql/domain/interfaces/gamesRepository.ts
@@ -0,0 +1,5 @@
+import { IPlayer } from '../entities/player';
+import { IPlayerWithRolls } from '../entities/rolls';
+import { PrismaClient } from '../../../../prisma/generated/client';
+
+export interface GamesRepository {}
